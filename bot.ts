ccfbf5f912ceea84739876ae09c214d1182c5231
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2024-02-26 17:41:41 -0500

    Set up basic bot structure

--- bot.ts
+++ bot.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+// Imports
+require('dotenv').config()
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+
+// Constants
+const token: string = (() : string => {
+  const t: string | undefined = process.env.TOKEN;
+  if (t === undefined) {
+    throw "Token variable isn't defined!";
+  } 
+  return t as string;
+})();
+
+const client = new Client({ intents: [
+  GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages
+]});
+
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
