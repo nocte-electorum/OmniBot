4262f0a6a0ed073c43c2c0797cac8b727c50c833
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2024-02-26 20:00:26 -0500

    Comment code better

--- bot.ts
+++ bot.ts
@@ -8,6 +8,8 @@ import { Client, Events, GatewayIntentBits } from 'discord.js'
 //-----------\\
 // Constants \\
 //-----------\\
+// Immediately called-then-defined function that
+// either reads TOKEN from the .env file or crashes
 const token: string = (() : string => {
   const t: string | undefined = process.env.TOKEN;
   if (t === undefined) {
@@ -16,6 +18,7 @@ const token: string = (() : string => {
   return t as string;
 })();
 
+// Set up client with specified intents (which information it'll receive)
 const client = new Client({ intents: [
   GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages
 ]});
@@ -24,8 +27,10 @@ const client = new Client({ intents: [
 //------------\\
 // Bot Events \\
 //------------\\
+// Function that runs only once on successful bot login
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+// Log in to the bot (actually puts it online)
 client.login(token);
