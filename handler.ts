1ef4b171080e391bcfda3d27d05f63f13f195f15
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2024-02-26 23:18:28 -0500

    Fix typo

--- handler.ts
+++ handler.ts
@@ -7,11 +7,10 @@ import fs from 'fs'
 // Set up command map
 const commands: Map<string, (args: string[]) => void> = new Map();
 
-
 // Populate command map with all modules in ./commands
 // this function assumes all modules have a default export which is the command function
 const command_files: string[] = fs.readdirSync("./commands")
-  .filter(file => file.endsWith(".ts") && file !== "template.ts`")
+  .filter(file => file.endsWith(".ts") && file !== "template.ts")
 
 for (const file in command_files) {
   const cmd_func: (args: string[]) => void = await import(file);
