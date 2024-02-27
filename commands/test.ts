c98d48b5b48958e4324bd77ae1a3d8849a8d5dca
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2024-02-27 11:37:08 -0500

    Complete test command

--- commands/test.ts
+++ commands/test.ts
@@ -1,7 +1,16 @@
+import { EmbedBuilder, Message } from "discord.js";
 import { Command } from "../command";
+import { OK_COLOR } from "../globals";
 
 const test: Command = new Command({ 
   name: "test"
 });
+
+test.func = (msg: Message, args: string[]) : void => {
+  const embed: EmbedBuilder = new EmbedBuilder()
+    .setColor(OK_COLOR)
+    .setDescription("Bot working!");
+  msg.channel.send({ embeds: [embed] })
+}
 
 export default test;
