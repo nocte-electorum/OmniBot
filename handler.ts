90e367d12b8a7aff7919d2f87a6dfebceba468c3
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2024-02-26 22:41:26 -0500

    Add dynamic command loading to `handler.ts`

---
+++ handler.ts
@@ -0,0 +1,19 @@
+/// File that reads all ts files in 
+/// ./commands and treats them as commonJS modules
+/// to be ran as commands
+
+import fs from 'fs'
+
+// Set up command map
+const commands: Map<string, (args: string[]) => void> = new Map();
+
+
+// Populate command map with all modules in ./commands
+// this function assumes all modules have a default export which is the command function
+const command_files: string[] = fs.readdirSync("./commands")
+  .filter(file => file.endsWith(".ts") && file !== "template.ts`")
+
+for (const file in command_files) {
+  const cmd_func: (args: string[]) => void = require(file);
+  commands.set(file, cmd_func);
+}
