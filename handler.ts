3898fe8191d4ad8e4aea010e8527686f294db337
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-04-27 19:23:45 -0400

    Export command map and run autofmt

--- handler.ts
+++ handler.ts
@@ -1,17 +1,18 @@
-/// File that reads all ts files in 
+/// File that reads all ts files in
 /// ./commands and treats them as commonJS modules
 /// to be ran as commands
 
-import fs from 'fs';
-import { Command } from './command';
+import fs from "fs";
+import { Command } from "./command";
 
 // Set up command map
-const commands: Map<string, Command> = new Map();
+export const commands: Map<string, Command> = new Map();
 
 // Populate command map with all modules in ./commands
 // this function assumes all modules have a default export which is the command function
-const command_files: string[] = fs.readdirSync("./commands")
-  .filter(file => file.endsWith(".ts"))
+const command_files: string[] = fs
+  .readdirSync("./commands") // read commands directory
+  .filter((file) => file.endsWith(".ts")); // only find files that end with .ts
 
 for (const file in command_files) {
   const cmd: Command = await import(file);
